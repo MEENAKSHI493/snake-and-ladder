478a9640e4ec061ebee3ed77dec438a64ab61765
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 23:41:21 +0530

    no play snake and ladder playing two playes

--- src/javasnakeandladder/snakeladder.java
+++ src/javasnakeandladder/snakeladder.java
@@ -3,39 +3,81 @@ package javasnakeandladder;
 public class snakeladder {
 
 	public static void main(String[] args) {
+		int count1 = 0;
 		int count = 0;
-		int Player = 0;
-		  while(Player<=99){
-			System.out.println("Player is at" + Player);
-			int Die=(int) (Math.random() * 10) % 6+1;
-				count =(count + 1);
-				System.out.println("Die value is at" + Die);
-			int Condition=(int) (Math.random() * 10) % 3+1;
-			//System.out.println("case number is " + Condition);
-			 switch(Condition) {
-			   case 1:
-			   System.out.println( "No Play You are in the same Player" );
-			   break;
-			  case 2:
-			   Player = (Player + Die);
-			   if(Player>100){
-				   Player = (Player - Die);
-				   continue;
-			   }
-			   System.out.println( "After ladder Player is at :" + Player );
-			   break;
-			   case 3: 
-			   Player = (Player - Die);
-			  
-			  if(Player<0){
-				  Player = 0;
-			  }
-				 System.out.println( "After snake Player is at :" + Player );
-			   break;
-			   }
-			    }
-				System.out.println( "Die play number " + count );
-			     }
-				  }
+	int Player1 = 0;
+	int Player2 = 0;
+	  while((Player1<=99)&&(Player2<=99)) {
+		//System.out.println("Player1 is " + Player1);
+		int Die1=(int) (Math.random() * 10) % 6+1;
+		int Die2=(int) (Math.random() * 10) % 6+1;
+			count =(count + 1);
+			count1 =(count1 + 1);
+			System.out.println("Die1 value is " +Die1);
+			System.out.println("Die2 value is " +Die2);
+		int type=(int) (Math.random() * 10) % 3+1;
+		int type1=(int) (Math.random() * 10) % 3+1;
+		//System.out.println("case number is " + type);
+		 switch(type) {
+		   case 1:
+		   System.out.println( "No Play You are in the same Player1" );
+		   break;
+		  
+		   case 2:
+		   Player1 = (Player1 + Die1);
+		   if(Player1>100){
+			   Player1 = (Player1 - Die1);
+			   continue;
+		   }
+		   System.out.println( "After ladder Player1 is at :" + Player1 );
+		   break;
+		   
+		   case 3: 
+		   Player1 = (Player1 - Die1);
+		  
+		  if(Player1<0){
+			  Player1 = 0;
+		  }
+			 System.out.println( "After snake Player1 is at :" + Player1 );
+		   break;
+		   }
+		   //2nd player
+		  switch(type1) {
+		   case 1:
+		   System.out.println( "No Play You are in the same Player2" );
+		   break;
+		  
+		   case 2:
+		   Player2 = (Player2 + Die2);
+		   if(Player2>100){
+			   Player2 = (Player2 - Die2);
+			   continue;
+		   }
+		   System.out.println( "After ladder Player2 is at :" + Player2 );
+		   break;
+		   
+		   case 3: 
+		   Player2 = (Player2 - Die2);
+		  
+		  if(Player2<0){
+			  Player2 = 0;
+		  }
+			 System.out.println( "After snake Player2 is at :" + Player2 );
+		   break;
+		   }
+		  
+		    }
+			
+			if(Player1 == 100){
+			System.out.println( "Player 1 is the Winner!");
+			System.out.println( "Die play number " + count );
+			}
+				else{
+					System.out.println( "Player 2 is the Winner!" );
+					System.out.println( "Die play number " + count1 );
+			}
+			System.out.println( "Die Total play number is " + (count1+count));
+			}
+		     }
 			    
 				
