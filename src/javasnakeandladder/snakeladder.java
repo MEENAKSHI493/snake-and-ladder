79044bf402e9d6aa33c83d7c168b72ef9af55302
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 23:07:21 +0530

    snake and ladder game

--- src/javasnakeandladder/snakeladder.java
+++ src/javasnakeandladder/snakeladder.java
@@ -3,7 +3,8 @@ package javasnakeandladder;
 public class snakeladder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("snake and ladder game");
+
 
 	}
 
