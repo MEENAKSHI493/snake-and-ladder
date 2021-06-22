5604fb571ccae17de45d92f50805fc9c8851d7e6
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 23:36:45 +0530

    no play snake and ladder winning position

--- src/javasnakeandladder/snakeladder.java
+++ src/javasnakeandladder/snakeladder.java
@@ -3,19 +3,20 @@ package javasnakeandladder;
 public class snakeladder {
 
 	public static void main(String[] args) {
+		int count = 0;
 		int Player = 0;
 		  while(Player<=99){
-			System.out.println("Player is at " + Player);
+			System.out.println("Player is at" + Player);
 			int Die=(int) (Math.random() * 10) % 6+1;
-				System.out.println("Die value is " + Die);
+				count =(count + 1);
+				System.out.println("Die value is at" + Die);
 			int Condition=(int) (Math.random() * 10) % 3+1;
 			//System.out.println("case number is " + Condition);
 			 switch(Condition) {
 			   case 1:
-			   System.out.println( "No Play You in same Player" );
+			   System.out.println( "No Play You are in the same Player" );
 			   break;
-			  
-			   case 2:
+			  case 2:
 			   Player = (Player + Die);
 			   if(Player>100){
 				   Player = (Player - Die);
@@ -23,18 +24,18 @@ public class snakeladder {
 			   }
 			   System.out.println( "After ladder Player is at :" + Player );
 			   break;
-			   
 			   case 3: 
 			   Player = (Player - Die);
 			  
 			  if(Player<0){
 				  Player = 0;
 			  }
-				 System.out.println( "After snake Player is at" + Player );
+				 System.out.println( "After snake Player is at :" + Player );
 			   break;
 			   }
-		  }
-	}
-	}
+			    }
+				System.out.println( "Die play number " + count );
+			     }
+				  }
 			    
 				
