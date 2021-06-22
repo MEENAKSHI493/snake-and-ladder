72344d8b0678d452fb9a5d8f9267e5e0385e4877
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 23:18:27 +0530

    snake and ladder game starting by dice value

--- src/javasnakeandladder/snakeladder.java
+++ src/javasnakeandladder/snakeladder.java
@@ -4,14 +4,12 @@ public class snakeladder {
 
 	public static void main(String[] args) {
 		int Position=0;
-		
-		
 		System.out.println("Welcome to Snake & Ladder Program Problem");
 		System.out.println("Your at Starting Position"+Position);
+		Dice();
 		}
-	
-
-
-	}
-
-
+		public static void Dice(){
+			int Dice = (int)Math.floor(Math.random()*10)%6+1;
+			System.out.println("Dice Value is "+Dice);
+		}
+		}
